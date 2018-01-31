a3d22d16c55d34da1b1eb3fa85c5cca6d19fae36
nandawteK <nandawtek@gmail.com>
2018-01-31 19:35:20 +0100

    avoid logic in presentation with helpers

--- app/helpers/katas_helper.rb
+++ app/helpers/katas_helper.rb
@@ -1,2 +1,7 @@
 module KatasHelper
+  NO_LINK = ''
+  def destroy_kata_link(id)
+    return link_to('Delete', kata_path(id), { method: :delete, data: { confirm: 'Are you sure?' }}) if user_signed_in?
+    NO_LINK
+  end
 end
