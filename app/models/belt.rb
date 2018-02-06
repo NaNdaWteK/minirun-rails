0ae69ffb80c17b1702507fd3a6b2ae4206867820
nandawteK <nandawtek@gmail.com>
2018-02-06 20:31:38 +0100

    With instance methods and static

--- app/models/belt.rb
+++ app/models/belt.rb
@@ -5,4 +5,13 @@ class Belt < ApplicationRecord
       belt = self.find(id)
       belt.kata.id
   end
+
+  def self.full_kata(id)
+    belt = self.find(id)
+    "#{belt.name} #{belt.description}"
+  end
+
+  def inline
+    "#{name} #{description}"
+  end
 end
--- spec/models/belt_spec.rb
+++ spec/models/belt_spec.rb
@@ -1,34 +1,37 @@
 require 'rails_helper'
 
 describe 'Belt' do
-  it 'belongs to a kata' do
-    kata = Kata.new(title: 'Kata potter', description: 'Pepe')
-    kata.save
+  before(:each) do
+    @kata = Kata.new(title: 'Kata potter', description: 'Pepe')
+    @kata.save
 
-    belt = Belt.new(
+    @belt = Belt.new(
       name: 'Bruce Lee',
       description: 'Bruce lee belt',
-      kata_id: kata.id
+      kata_id: @kata.id
     )
-    belt.save
+    @belt.save
+  end
 
-    expect(belt.kata.id).to eq(kata.id)
-    expect(kata.belts).to eq([belt])
+  it 'belongs to a kata' do
+    expect(@belt.kata.id).to eq(@kata.id)
+    expect(@kata.belts).to eq([@belt])
   end
 
   it "retrieves its kata id" do
-    kata = Kata.new(title: 'Kata potter', description: 'Pepe')
-    kata.save
 
-    belt = Belt.new(
-      name:'name',
-      description: 'Description',
-      kata_id: kata.id
-    )
-    belt.save
+    retrieved_kata_id = Belt.find_kata_id(@belt.id)
 
-    retrieved_kata_id = Belt.find_kata_id(belt.id)
+    expect(retrieved_kata_id).to eq(@kata.id)
+  end
 
-    expect(retrieved_kata_id).to eq(kata.id)
+  it "retrieves name with its description" do
+
+    full_kata = Belt.full_kata(@belt.id)
+
+    expect(full_kata).to eq("#{@belt.name} #{@belt.description}")
+  end
+  it "retrieves name with its description" do
+    expect(@belt.inline).to eq("#{@belt.name} #{@belt.description}")
   end
 end
