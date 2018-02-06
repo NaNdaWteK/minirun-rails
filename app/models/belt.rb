09de1b178cd7b05d479319876cbaeccf4c5c9564
nandawteK <nandawtek@gmail.com>
2018-02-06 20:15:56 +0100

    Create static methos for mantain logic in model

--- app/models/belt.rb
+++ app/models/belt.rb
@@ -1,3 +1,8 @@
 class Belt < ApplicationRecord
   belongs_to :kata
+
+  def self.find_kata_id(id)
+      belt = self.find(id)
+      belt.kata.id
+  end
 end
--- spec/models/belt_spec.rb
+++ spec/models/belt_spec.rb
@@ -15,4 +15,20 @@ describe 'Belt' do
     expect(belt.kata.id).to eq(kata.id)
     expect(kata.belts).to eq([belt])
   end
+
+  it "retrieves its kata id" do
+    kata = Kata.new(title: 'Kata potter', description: 'Pepe')
+    kata.save
+
+    belt = Belt.new(
+      name:'name',
+      description: 'Description',
+      kata_id: kata.id
+    )
+    belt.save
+
+    retrieved_kata_id = Belt.find_kata_id(belt.id)
+
+    expect(retrieved_kata_id).to eq(kata.id)
+  end
 end
